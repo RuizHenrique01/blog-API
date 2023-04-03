fb1aaf6ec7ec6189937dee7ea8e82d64a7183723
Affonso Henrique Ruiz Jurema <affonsohenriqueruiz@gmail.com>
2023-04-03 09:24:23 -0400

    feat: adding delete and update routes

--- blog/main.py
+++ blog/main.py
@@ -22,6 +22,28 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog {id} not found.")
+    
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {'message': 'Success delete'}
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog {id} not found.")
+    
+    blog.update(request.dict())
+    db.commit()
+    return { 'message': 'updated' }
+
 @app.get('/blog', status_code=status.HTTP_200_OK)
 def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
